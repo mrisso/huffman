52dc22462c605beaf84174e561cc064f0c182867
mrisso <matheush.risso@gmail.com>
2016-06-26 03:18:31 -0300

    Código de criação e free de lista duplamente encadeada circular

--- Lista.c
+++ Lista.c
@@ -17,11 +17,61 @@ typedef struct lista{
 	struct lista* Prox;
 }tLista;
 
-tLista* CriaLista()
+tLista* InitLista(void)
+{
+	return NULL;
+}
+
+tLista* CriaItem(char letra, int freq) // Recebe a letra e a frequência do item e já inicializa
 {
 	tLista* lista = (tLista*)malloc(sizeof(tLista));
 	lista->Ant = NULL;
 	lista->Prox = NULL;
-	lista->Item.Letra = 0;
-	lista->Item.Freq = 0;
+	lista->Item.Letra = letra; // <-
+	lista->Item.Freq = freq; // <-
+}
+
+void InsereItem(tLista **lista, tLista *novoItem)
+{
+	tLista *andador = *lista;
+
+	if(andador==NULL) //Caso lista vazia
+	{
+		// -> Lista de item único <-
+		*lista=novoItem; //Novo item é o primeiro item
+		novoItem->Ant = *lista; //Novo item é o anterior (lista circular)
+		novoItem->Prox = *lista; //Novo item é o próximo (lista circular)
+	}
+	
+	else
+	{
+		while(andador->Prox!=*lista)
+			andador=andador->Prox; //Ir até o fim da lista
+		andador->Prox = novoItem; //Adicionar ao fim da lista
+		novoItem->Ant = andador; //Campo anterior do novo item é o antigo útlimo item
+		(*lista)->Ant = novoItem; //Campo anterior do primeiro item da lista aponta para o último item (novo)
+	}
+
+	novoItem->Prox=*lista; //Último Item da Lista aponta para o primeiro
+
+}
+
+void freeLista(tLista *lista)
+{
+	tLista *aux, *prox;
+
+	aux = lista; //Recebe primeira posição
+
+	prox = aux->Prox;
+	free(aux); //Libera primeira posição
+	aux = prox;
+
+	/* Libera até que o campo Prox da célula visitada seja igual ao endereço da primeira posição da lista
+	 * (último elemento da lista aponta para o primeiro por ser uma lista circular) */
+	while(aux!=lista)
+	{
+		prox = aux->Prox;
+		free(aux);
+		aux = prox;
+	}
 }
