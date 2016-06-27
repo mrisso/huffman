a0b9f77f27d863b5dcdb09ecad5c7b70ec4e6f58
gabrieldallabf <gabrieldalla.b@gmail.com>
2016-06-27 15:23:30 -0300

    Entrada do arquivo feita e copia dados para string com tamanho exato do arquivo

---
+++ Compacta.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+long TamanhoArquivo(const char *Arq_in);
+void LeArquivo(FILE* entrada, char* Vetor);
+
+int main(int argc, char** argv)
+{
+	const char *Arq_in = "teste.txt";
+	long tamanho = TamanhoArquivo(Arq_in);
+	FILE* entrada;
+	
+	if(argc != 2)
+	{
+		printf("Arquivo de entrada não fornecido\n");
+		return 0;
+	}
+	entrada = fopen(argv[1], "rt");
+	if(entrada == NULL)
+	{
+		printf("Erro na abertura do arquivo\n");
+		return 0;
+	}
+	int i=0;
+	char Vetor[tamanho];
+	printf("%ld\n", tamanho);
+	LeArquivo(entrada, Vetor);
+
+	fclose(entrada);
+	return 0;
+}
+long TamanhoArquivo(const char *Arq_in)
+{
+  long Tamanho;
+  FILE *arquivo = fopen(Arq_in, "rb");
+  fseek(arquivo, 0, SEEK_END);
+  Tamanho = ftell(arquivo);
+  fclose(arquivo);
+  return Tamanho;
+}
+void LeArquivo(FILE* entrada, char* Vetor)
+{
+	while((fgets(Vetor, sizeof(Vetor), entrada)) != NULL)
+		printf("%s", Vetor);
+}	
