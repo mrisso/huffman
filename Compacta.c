2593a73da52fd207b611f525a94a3fdcd63f557c
Gabriel Dalla Bernardina Fagundes <gabrieldalla.b@gmail.com>
2016-06-27 22:48:16 -0300

    Vetor da estrutura TipoDados feito e armazenando a letra e sua frequencia
    está dando um erro ao compilar, algo de "referencia indefinida" para as funções que vem do TAD lista.h tentei procurar uma solução mas não achei nada...

--- Compacta.c
+++ Compacta.c
@@ -5,30 +5,40 @@
 #include "Lista.h"
 
 long TamanhoArquivo(const char *Arq_in);
-void LeArquivo(FILE* entrada, char* Vetor);
+char* LeArquivo(FILE* entrada, char* Vetor, long tamanho);
+void CriaLista(TipoDados *vetor, int tam, tLista **lista);
+TipoDados* ArmazenaLetras(char* String, long tamanho);
 
 int main(int argc, char** argv)
 {
 	const char *Arq_in = "teste.txt";
 	long tamanho = TamanhoArquivo(Arq_in);
-	FILE* entrada;
 	
-	if(argc != 2)
-	{
+	FILE* entrada;
+	if(argc != 2){
 		printf("Arquivo de entrada não fornecido\n");
 		return 0;
 	}
 	entrada = fopen(argv[1], "rt");
-	if(entrada == NULL)
-	{
+	if(entrada == NULL){
 		printf("Erro na abertura do arquivo\n");
 		return 0;
 	}
+
 	int i=0;
 	char Vetor[tamanho];
+	char* string;
+	TipoDados* Dados;
 	printf("%ld\n", tamanho);
-	LeArquivo(entrada, Vetor);
+	string = LeArquivo(entrada, Vetor, tamanho);
+//printf("%s\n", string);
+	Dados = ArmazenaLetras(Vetor, tamanho);
 
+	for(i=0; i<255; i++)
+	{
+		if(Dados[i].Letra != '\0' && Dados[i].Freq != 0)
+			printf("%c - %d\n", Dados[i].Letra, Dados[i].Freq);
+	}
 	fclose(entrada);
 	return 0;
 }
@@ -43,16 +53,47 @@ long TamanhoArquivo(const char *Arq_in)
   return Tamanho;
 }
 
-void LeArquivo(FILE* entrada, char* Vetor)
+char* LeArquivo(FILE* entrada, char* Vetor, long tamanho)
 {
-	while((fgets(Vetor, sizeof(Vetor), entrada)) != NULL)
-		printf("%s", Vetor);
-}	
+	while(!feof(entrada))
+		return fgets(Vetor, tamanho, entrada);
+}
 
+TipoDados* ArmazenaLetras(char* String, long tamanho)
+{
+	TipoDados* Vetor = (TipoDados*)malloc(256*sizeof(TipoDados)); // Vetor com o tamanho da AscII
+	int i, j, k=0;
+	int freq=0;
+	for(i=0; i<255; i++)
+	{
+		for(j=0; j<(tamanho-1); j++)
+		{
+			if(String[j] == i)
+				freq++;
+		}
+		
+		if(freq != 0)
+		{
+			Vetor[k].Letra = i;
+			Vetor[k].Freq = freq;
+			k++;
+		}
+		else
+		{
+			Vetor[k].Letra = '\0';
+			Vetor[k].Freq = 0;	
+		}
+		
+		freq = 0;
+	}
+	return Vetor;
+}
 void CriaLista(TipoDados *vetor, int tam, tLista **lista)
 {
 	int contador;
 
-	for(contador=0;contador<tam;contador++)			
-		InsereItem(lista,InitItem(vetor[contador]));
+	for(contador=0;contador<tam;contador++)
+	{		
+		InsereItem(lista, InitItem(vetor[contador]) );
+	}
 }
